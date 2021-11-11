ed321b87430c439c633991f17c8e594963305f9d
Daniel Lopez E <daniel.lopez@proveedores.lineadirecta.com.co>
2021-11-10 23:03:53 -0500

    Se agrega documentación

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Se importa librería que crea un servidor
 let express = require("express");
+// Se instancia la librería express
 let app = express();
-const PORT = 3000; //Puerto a usar para abrir el servidor
+// Se define puerto de ejecución
+const PORT = 3000;
+// Se establece ubicación de la carpeta del servidor
 app.use(express.static("./WebSite"));
+// Se abre el puerto
 app.listen(PORT, function() {
     console.log(`Listening at http://localhost:${PORT}`);
 });
